ebe37282fbc5a9e51a6f17fa279376452646b580
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-05-24 18:34:21 +0000

    ##

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Yoochan
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+window.onload = function CaseTest() {
+
+  const params = new URLSearchParams(document.location.search)
+  //input with URL
+  const pi = "π"
+  const radius = parseFloat(params.get("radius"))
+  const unit = params.get("unit")
+
+  //process
+  let ans = 4 / 3 * ((radius ** 3).toFixed(2))
+  document.getElementById("get_radius").innerHTML = radius
+  document.getElementById("get_unit").innerHTML = unit
+  document.getElementById("Ans").innerHTML = "V = " + ans.toFixed(2) + pi + unit
+}
